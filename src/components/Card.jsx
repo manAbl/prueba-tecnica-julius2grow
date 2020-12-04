9a722234cdb45940523d5f1b08cf777e6f818110
manAbl <manuel.22.lamas@gmail.com>
2020-12-03 21:36:16 -0500

    Card Simplified

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import PropTypes from 'prop-types';
 
-const useStyles = makeStyles({
-  root: {
-    width: '100%'
-  },
-  title: {
-    fontSize: 17.2,
-    fontWeight: 600,
-    marginBottom: '1.2em',
-    display: 'inline-block',
-  },
-  pos: {
-    marginBottom: 8.94,
-  },
-});
-
 const ICard = ({ children }) => {
-  const classes = useStyles();
   return (
-    <Card className={classes.root}>
+    <Card>
       <CardContent>
         {children}
       </CardContent>
@@ -31,7 +14,7 @@ const ICard = ({ children }) => {
 };
 
 Card.propTypes = {
-  title: PropTypes.string,
+  children: PropTypes.element.isRequired
 };
 
 export default ICard;
